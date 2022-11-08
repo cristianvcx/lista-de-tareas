e524940846f7fe00c9a5157ceefecbb665a68b75
cristian <cristianvcx188@gmail.com>
2022-11-08 19:10:07 -0300

    tarea

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { isEmpty, size } from 'lodash'
+import shortid from 'shortid'
 
 function App() {
+  const [task, setTask] = useState("")
+  const [tasks, setTasks] = useState([])
+
+  const addTask = (e) => {
+    e.preventDefault()
+    if(isEmpty(task)){
+      console.log("Task empty")
+      return
+    }
+
+    const newTask = {
+      id: shortid.generate(),
+      name: task
+    }
+
+    setTasks([ ...tasks, newTask ])
+    setTask("")
+  }
+
+  const deleteTask = (id) => {
+    const filteredTasks = tasks.filter(task => task.id !== id)
+    setTasks(filteredTasks)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1>Tareas</h1>
+      <hr/>
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">Lista de Tareas</h4>
+          {
+            size(tasks) === 0 ? (
+              <h5 className='text-center'>Aun no hay tareas programadas.</h5>
+            ) : (
+              <ul className="list-group">
+              {
+                tasks.map((task) => (
+                <li className="list-group-item" key={task.id}>
+                  <span className="lead">{task.name}</span>
+                  <button 
+                    className="btn btn-danger btn-sm float-right mx-2"
+                    onClick={() => deleteTask(task.id)}
+                  >
+                    Eliminar
+                  </button>
+                  <button 
+                    className="btn btn-warning btn-sm float-right">
+                    Editar
+                  </button>
+                </li>
+                ))
+              }
+            </ul>
+            )
+          }
+        </div>
+        <div className="col-4">
+          <h4 className="text-center">Formulario</h4>
+          <form onSubmit={addTask}>
+            <input
+              type="text"
+              className="form-control mb-2"
+              placeholder="Ingrese la tarea"
+              onChange={(text) => setTask(text.target.value)}
+              value={task}
+            />
+            <button
+              className="btn btn-dark btn-block"
+              type="submit"
+            >
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
